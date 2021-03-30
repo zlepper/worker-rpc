a54c019579415416c78aca1e7363993b43378389
Zlepper <hansen13579@gmail.com>
2021-03-30 20:26:36 +0200

    Kill worker when test is done

--- tests/nodejs/tests.spec.ts
+++ tests/nodejs/tests.spec.ts
@@ -3,7 +3,7 @@ import { Worker } from 'worker_threads';
 import { WorkerThreadClientConnection, wrapBackgroundService, WrappedObject } from '../../src';
 import { Calculator } from '../shared/calculator';
 
-function create(): WrappedObject<Calculator> {
+function create(): [WrappedObject<Calculator>, Worker] {
   const workerThread = new Worker(`
   const wk = require('worker_threads');
   require('ts-node').register();
@@ -17,29 +17,33 @@ function create(): WrappedObject<Calculator> {
   });
   const workerConnection = new WorkerThreadClientConnection(workerThread);
 
-  return wrapBackgroundService<Calculator>(workerConnection);
+  return [wrapBackgroundService<Calculator>(workerConnection), workerThread];
 }
 
 describe('NodeJS', function() {
-  it('should invoke normal (non-promise) methods', async function() {
-    const calculator = create();
+  let calculator: WrappedObject<Calculator>;
+  let worker: Worker;
 
+  beforeEach(function() {
+    [calculator, worker] = create();
+  })
+
+  it('should invoke normal (non-promise) methods', async function() {
     const result = await calculator.add(1, 2);
     expect(result).to.equal(3);
   });
 
   it('should invoke promise methods', async function() {
-    const calculator = create();
-
     const result = await calculator.addUsingPromise(3, 4);
     expect(result).to.equal(7);
   });
 
   it('should invoke methods with "complex" type arguments', async function() {
-    const calculator = create();
-
     const result = await calculator.addAll([1, 2, 3, 4]);
-
     expect(result).to.equal(10);
   });
+
+  afterEach(function() {
+    return worker.terminate();
+  })
 });
